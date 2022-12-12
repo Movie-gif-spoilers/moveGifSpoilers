ad0fabd83d4695f91ed78a7f623af6175f99b8fe
junior-brempong <juniorwilliam222@gmail.com>
2022-12-12 16:53:14 -0500

    Netlify troubleshoot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import react from 'react';
+// import react from 'react';
 import Header from './components/Header.js';
 import DisplayGifs from './components/DisplayGifs.js';
 import SearchBar from './components/SearchBar';
 import Footer from './components/Footer.js';
+// import axios from 'axios';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
      <Header />
@@ -19,3 +24,14 @@ function App() {
 }
 
 export default App;
+
+
+// App.js (State for both API calls) 
+// a. Take the user input from search bar
+// i. Store that in user input in useState
+// b. Await Axios (make a call to the movie API’s)
+// i. Return movieID
+// c. Call “get keywords” endpoint
+// i. Get an array of keywords
+// ii. Hold the array in state 
+// iii. Pass that array as a prop to the display gif component
