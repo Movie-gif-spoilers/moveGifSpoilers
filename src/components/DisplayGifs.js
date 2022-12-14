fee1e31d6f1e7cebf046468ef941a2759b5016a2
junior-brempong <juniorwilliam222@gmail.com>
2022-12-14 17:40:10 -0500

    Added in changes

--- src/components/DisplayGifs.js
+++ src/components/DisplayGifs.js
@@ -6,44 +6,28 @@ import { useEffect, useState } from 'react';
 const DisplayGifs = (props) => {
 	const [currentGifs, setCurrentGifs] = useState([]);
 
-	
-	// Store the keywords we recieve from props into a variable and then pass this through to our q. We will need splice this array of keywords to have 3 and then loop through this for 3 seperate API calls
 	const keywords = props.keywords;
 
-	console.log('these are the keywords we can loop through', keywords);
+	console.log('keywords', keywords);
 
 	const newGifsArray = [];
+
 	useEffect(() => {
-			axios({
 		keywords.forEach(async (keyword) => {
 			axios({
 				url: `https://api.giphy.com/v1/gifs/search`,
 				params: {
 					api_key: `NFjbXVR8Fnr6sKnvC2hgL2etOmY2z7hO`,
-					q: `${keywords}`,
-					limit: 10,
 					q: keyword,
 					limit: 3,
 				},
-			})
-			.then((res) => {
-				// We take back the first three items we recieve in the array
-				setCurrentGifs(res.data.data[]);
-			})
+			}).then((res) => {
+				console.log('giphy data', res.data.data);
+				setCurrentGifs(res.data.data);
+			});
+		});
 	}, [props.keywords]);
 
-	// let gifs = newGifsArray.map((gif, index) => {
-	// 	console.log('gif being mapped', gif);
-	// 	return (
-	// 		<li key={gif[0].id}>
-	// 			<img
-	// 				src={`https://media.giphy.com/media/${gif[0].id}/giphy.gif`}
-	// 				alt={gif[0].title}
-	// 			/>
-	// 		</li>
-	// 	);
-	// });
-	// setCurrentGifs(gifs);
 	console.log('newGifsarray', newGifsArray);
 	console.log('current gifs', currentGifs);
 
