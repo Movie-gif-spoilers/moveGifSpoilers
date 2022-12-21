23a3da4fbb7bcb10c0a53485e176c938def0c7ac
rebecca-romeo <rebecca.romeo@ryerson.ca>
2022-12-21 01:31:49 -0500

    media queries

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -49,7 +49,7 @@ const SearchBar = (props) => {
 					Enter a movie and press play
 				</label>
 				<input
-					placeholder="Enter a movie and press play"
+					placeholder="Enter a movie & press play"
 					onChange={userChoice}
 					type="text"
 					id="userMovieChoice"
