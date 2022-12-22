88ca1236157699c9e30e7cffb3f5e719e9843004
junior-brempong <juniorwilliam222@gmail.com>
2022-12-21 19:30:46 -0500

    Pseudo for displaygifs

--- src/components/DisplayGifs.js
+++ src/components/DisplayGifs.js
@@ -6,25 +6,31 @@ import SearchBar from './SearchBar';
 // import {homeNavBar} from "./NavBar.js";
 
 const DisplayGifs = (props) => {
-	// eslint-disable-next-line
+	
+	// We should our shuffle or keys we get back from the randomizer function in state.
 	const [shuffle, setShuffle] = useState([]);
 
+
+	// A function used to shuffle the through an array, passing in the paramaters or array and number. We use Math.random to select a random assortment from our array and the return our shuffled sliced from the beginning of the array to the end.
 	function getMultipleRandom(arr, num) {
 		const shuffled = [...arr].sort(() => 0.5 - Math.random());
 		return shuffled.slice(0, num);
 	}
 
+	// A function we pass through to our onClick on our button which then resets of shuffle to a new array of randomized keywords
 	function handleRandomize(e) {
 		setShuffle(getMultipleRandom(props.keywords, 10));
-		console.log(e);
 	}
 
+	// A variable we use to grab 4 random keywords. This is what we will use to map through our return to render of Gif component, since the gifs displayed will be based off of these 4 keywords.
 	const keywords = getMultipleRandom(props.keywords, 4);
 
+	// We create this function here and pass it through as a prop to our Gif Component. This will change the stare of our setKeywords when we click on an image. This is in turn populates new gifs based off of the keyword of that particular image
 	const handleGifClick = (gifKeyword) => {
 		props.setKeywords([gifKeyword]);
 	};
 
+	// A ternary operate for displaying our welcome messaged based off the length of the keywords we recieve.
 	const WelcomeMessage = () => {
 		return props.keywords.length === 0 ? (
 			<h3 className="welcomeH3 wrapper">
@@ -65,6 +71,7 @@ const DisplayGifs = (props) => {
 
 				<div className="randomAndSave wrapper">
 					<div>
+						{/* A ternary operator that displays our button when recieved a keyword, but then removes the button once a user has clicked the image to find more gifs based off a particular keyword */}
 						{keywords.length === 1 || keywords.length === 0 ? null : (
 							<button
 								className="shuffleButton"
@@ -75,6 +82,7 @@ const DisplayGifs = (props) => {
 						)}
 					</div>
 
+					{/* Ternary operator for displaying our favorite buttons only if a keyword has been given */}
 					<div className="favesButton">
 						{keywords.length === 0 ? null : <Favourites />}
 					</div>
